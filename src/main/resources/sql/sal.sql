72ed8019ca891ce6a41621e4ca9e9fdb7504efdb
java <java>
2017-03-21 14:44:49 +0900

    sal.sql

--- src/main/resources/sql/sal.sql
+++ src/main/resources/sql/sal.sql
@@ -1,7 +1,7 @@
 drop table sal;
 
 drop sequence sal_salno_seq;
-create sequence sal_salno_seq start with 1;
+create sequence sal_salno_seq start with 10;
 
 create table sal(
 	salno      		number(3)   	constraint pk_sal primary key,
@@ -9,8 +9,10 @@ create table sal(
    	overtime_sal   	number(7,2),
    	comm      		number(7,2),
    	aid_sal      	number(7,2),
-   	sal_date   		date,
-   	membersrl      	number(10)    	constraint fk_membersrl references member(membersrl) on delete set null
+   	sal_date   		number(2)		not null,
+   	membersrl      	number(10)    	constraint fk_membersrl0 references member(membersrl) on delete set null
 );
+
+insert into sal values (sal_salno_seq.nextval, 500, null, null, null, 20, 1);
 
 commit;
